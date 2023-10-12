d3b920d3fb523a1faac2f73ce79e207fbb36dc35
luca-masera <lucamasera1995@gmail.com>
2023-10-12 15:46:32 +0200

    marco

--- Javascript/script.js
+++ Javascript/script.js
@@ -15,27 +15,36 @@ const lista = [
 
 button.addEventListener('click',function(){
     let email = document.getElementById('email').value;
+  
+
     console.log(email)
     
     for(let i = 0; i < lista.length; i++){
         let currentEmail = lista[i];
         console.log(currentEmail)
+
+        
         
         let write
 
         if(currentEmail.toLocaleLowerCase() === email.toLocaleLowerCase()){
             write = 'Account verificato, puoi accedere al sito.'
+            
             console.log(write)
+            break;
+            
             
         } if(currentEmail.toLocaleLowerCase() !== email.toLocaleLowerCase()){
             write = 'Account non trovato.'
             console.log(write)
+            
         }
+        
+        
+    }
 
         const scritta = document.querySelector('p')
         scritta.innerText = write;
-    }
-
 
 })
 
