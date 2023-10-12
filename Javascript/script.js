35bb8040121e35a7cf48c6a38a99b26d981d8aa8
luca-masera <lucamasera1995@gmail.com>
2023-10-12 14:55:26 +0200

    for per la lista e if se c'è o non c'è

--- Javascript/script.js
+++ Javascript/script.js
@@ -17,7 +17,22 @@ button.addEventListener('click',function(){
     let email = document.getElementById('email').value;
     console.log(email)
     
-    for(let i = 0; i <lista.length; i++){}
+    for(let i = 0; i < lista.length; i++){
+        let currentEmail = lista[i];
+        console.log(currentEmail)
+        
+        let write;
+
+        if(currentEmail === email){
+            write = 'Account verificato, puoi accedere al sito.'
+            console.log(write)
+            
+        } if(currentEmail !==email){
+            write = 'Account non trovato.'
+            console.log(write)
+        }
+
+    }
 
 
 })
