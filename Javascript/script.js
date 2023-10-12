619d9c6ddb52535185d3e6d099dca23f0f0bceab
luca-masera <lucamasera1995@gmail.com>
2023-10-12 14:34:16 +0200

    Chiediamo email

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+const button = document.querySelector('button');
+
+const lista = [
+    'Giacomo@gmail.com',
+    'Michele@gmail.com',
+    'Fernando@gmail.com',
+    'Luigi@gmail.com',
+    'Andrea@gmail.com',
+    'Fiorello@gmail.com'
+];
+
+button.addEventListener('click',function(){
+    let email = document.getElementById('email').value;
+    console.log(email)
+    
+    for(let i = 0; i <lista.length; i++){}
+
+
+})
+
+
